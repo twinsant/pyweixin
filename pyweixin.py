25760089737fbb69c322c693d1e95a76051f1b18
twinsant <twinsant@gmail.com>
2025-11-23 17:52:00 +0800

    Add: create draft for mp.

--- pyweixin.py
+++ pyweixin.py
@@ -1,8 +1,11 @@
 # -*- coding: utf8
 # Author: twinsant@gmail.com
 import hashlib
+import json
 import xml.etree.ElementTree as ET
 from functools import cmp_to_key
+
+import requests
 
 class WeiXin(object):
     def __init__(self, token=None, timestamp=None, nonce=None, signature=None, echostr=None, xml_body=None):
@@ -100,3 +103,76 @@ class WeiXin(object):
         a = sorted([v for k, v in params.items()])
         s = ''.join(a)
         return hashlib.sha1(s.encode('utf-8')).hexdigest()
+
+    @staticmethod
+    def get_access_token(appid, secret):
+        """获取微信公众号 access_token
+        
+        接口文档: https://developers.weixin.qq.com/doc/subscription/api/base/api_getaccesstoken.html
+        
+        Args:
+            appid: 微信公众号的 AppID
+            secret: 微信公众号的 AppSecret
+        
+        Returns:
+            dict: 包含 access_token 和 expires_in 的字典
+                  成功: {"access_token": "ACCESS_TOKEN", "expires_in": 7200}
+                  失败: {"errcode": 错误码, "errmsg": "错误信息"}
+        """
+        if not appid or not secret:
+            return {"errcode": -1, "errmsg": "appid 或 secret 未配置"}
+        
+        url = 'https://api.weixin.qq.com/cgi-bin/token'
+        params = {
+            'grant_type': 'client_credential',
+            'appid': appid,
+            'secret': secret
+        }
+        
+        response = requests.get(url, params=params)
+        return response.json()
+
+    @staticmethod
+    def create_draft(access_token, title, digest, content, 
+                    content_source_url='', thumb_media_id=None, author=''):
+        """创建微信公众号草稿
+        
+        接口文档: https://developers.weixin.qq.com/doc/subscription/api/draft/draft_add.html
+        
+        Args:
+            access_token: 微信公众号 access_token
+            title: 文章标题
+            digest: 文章摘要
+            content: 文章内容(HTML格式)
+            content_source_url: 原文链接(可选)
+            thumb_media_id: 封面图片的media_id(可选)
+            author: 文章作者(可选)
+        
+        Returns:
+            dict: 微信API返回的结果
+                  成功: {"media_id": "MEDIA_ID"}
+                  失败: {"errcode": 错误码, "errmsg": "错误信息"}
+        """
+        url = f'https://api.weixin.qq.com/cgi-bin/draft/add?access_token={access_token}'
+        
+        article = {
+            "title": title,
+            "author": author,
+            "digest": digest,
+            "content": content,
+            "content_source_url": content_source_url,
+            "need_open_comment": 0,
+            "only_fans_can_comment": 0
+        }
+        
+        if thumb_media_id:
+            article['thumb_media_id'] = thumb_media_id
+        else:
+            raise ValueError("thumb_media_id 不能为空")
+        
+        data = {"articles": [article]}
+        data_to_send = json.dumps(data, ensure_ascii=False)
+        
+        response = requests.post(url, data=data_to_send, 
+                                headers={'Content-Type': 'application/json'})
+        return response.json()
---
+++ test_mp.py
@@ -0,0 +1,26 @@
+import os
+from dotenv import load_dotenv
+
+from pyweixin import WeiXin
+
+load_dotenv()
+
+MP_APP_ID = os.getenv('MP_APP_ID')
+MP_APP_SECRET = os.getenv('MP_APP_SECRET')
+THUMB_MEDIA_ID = os.getenv('THUMB_MEDIA_ID')
+
+if __name__ == '__main__':
+    # 获取access_token
+    result = WeiXin.get_access_token(appid=MP_APP_ID, secret=MP_APP_SECRET)
+    access_token = result.get('access_token')
+
+    # 创建草稿
+    draft = WeiXin.create_draft(
+        access_token=access_token,
+        title='文章标题',
+        digest='文章摘要',
+        content='<p>文章内容</p>',
+        author='作者名',
+        thumb_media_id=THUMB_MEDIA_ID,
+    )
+    print(draft)
